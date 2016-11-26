c65c2a1a8aa924ea93056b836fdf6e3e3c733eca
Robin Southern <betajaen@users.noreply.github.com>
2016-11-26 19:49:50 +0000

    Section rendering and collisions

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-#define RETRO_WINDOW_CAPTION "Dragons"
-#define RETRO_ARENA_SIZE Kilobytes(32)
-#define RETRO_WINDOW_DEFAULT_WIDTH 1280
-#define RETRO_WINDOW_DEFAULT_HEIGHT 560
-#define RETRO_CANVAS_DEFAULT_WIDTH (RETRO_WINDOW_DEFAULT_WIDTH / 2)
-#define RETRO_CANVAS_DEFAULT_HEIGHT (RETRO_WINDOW_DEFAULT_HEIGHT / 2)
+
+#include "retro.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
 
 #define TILE_SIZE 16
 #define SECTION_WIDTH   (RETRO_CANVAS_DEFAULT_WIDTH / TILE_SIZE)
@@ -11,42 +12,20 @@
 
 #define LEVEL_WIDTH (SECTION_WIDTH * 2)
 #define LEVEL_HEIGHT (SECTION_HEIGHT)
-#define MAX_OBJECTS_PER_SECTION 16
-#define SECTIONS_PER_LEVEL (4)
+#define SECTION_COUNT 4
 
 #define MAX_MONSTERS 256
 
 
 #define VERSION "1.0"
 
-#include "retro.c"
 
 static Font           FONT_NEOSANS;
 static Bitmap         SPRITESHEET;
 static Bitmap         SPRITESHEET_HUMANOID[2];
 static Bitmap         SPRITESHEET_REPTILE[2];
-static Bitmap         CATSHEET;
-static Bitmap         PLAYERSHEET;
-static Bitmap         COCO;
-static Animation      ANIMATEDSPRITE_PLAYER_IDLE;
-static Animation      ANIMATEDSPRITE_PLAYER_WALK;
-static Animation      ANIMATEDSPRITE_CAT1_IDLE;
-static Animation      ANIMATEDSPRITE_CAT1_WALK;
-static Animation      ANIMATEDSPRITE_CAT2_IDLE;
-static Animation      ANIMATEDSPRITE_CAT2_WALK;
-static Animation      ANIMATEDSPRITE_CAT3_IDLE;
-static Animation      ANIMATEDSPRITE_CAT3_WALK;
-static Animation      ANIMATEDSPRITE_CAT_SHADOW_IDLE;
-static Animation      ANIMATEDSPRITE_CAT_SHADOW_WALK;
-static Sound          SOUND_JUMP1;
-static Sound          SOUND_JUMP2;
-static Sound          SOUND_HURT1;
-static Sound          SOUND_HURT2;
-static Sound          SOUND_PICKUP1;
-static Sound          SOUND_PICKUP2;
-static Sound          SOUND_SELECT;
-static Bitmap         TILES1;
-
+static Bitmap         SPRITESHEET_WALLS;
+static Bitmap         SPRITESHEET_FLOORS;
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -73,8 +52,104 @@ typedef enum
 u32 randr(u32 min, u32 max)
 {
   double scaled = (double)rand()/RAND_MAX;
+  return (u32) ((max - min + 1)*scaled + min);
+}
 
-  return (max - min +1)*scaled + min;
+typedef struct
+{
+  U16   col[SECTION_WIDTH * SECTION_HEIGHT];
+  U16   non[SECTION_WIDTH * SECTION_HEIGHT];
+} SectionData;
+
+SectionData* SECTION_DATA;
+U32 SECTION_DATA_COUNT;
+
+char* skipWhitespace(char* s)
+{
+  while(*s != 0 && isspace(*s))
+    s++;
+  return s;
+}
+
+char* skipToDigit(char* s)
+{
+  while(*s != 0 && !isdigit(*s))
+    s++;
+  return s;
+}
+
+char* skipString(char* s, const char* str)
+{
+  s = skipWhitespace(s);
+  s += strlen(str);
+  s = skipWhitespace(s);
+  return s;
+}
+
+bool skipToString(char* s, char** t, const char* str)
+{
+  (*t) = strstr(s, str);
+  if ((*t) == NULL)
+    return false;
+  return true;
+}
+
+char* readUInt(char* s, U32* i)
+{
+  (*i) = 0;
+
+  while(*s != 0 && isdigit(*s))
+  {
+    (*i) = (*i) * 10  + ((*s) - '0');
+    s++;
+  }
+
+  return s;
+}
+
+void LoadSectionData()
+{
+  SECTION_DATA = malloc(sizeof(SectionData) * SECTION_COUNT);
+
+  U8 sectionIndex = 0;
+
+  U32 dataSize;
+  char* data = TextFile_Load("sections.tmx", &dataSize);
+
+  if (data == NULL)
+    printf("Bad text file\n");
+
+  while(*data != 0)
+  {
+    char* t;
+    SectionData* section = &SECTION_DATA[sectionIndex++];
+    if (skipToString(data, &t, "\"csv\">"))
+    {
+      data = skipString(t, "\"csv\">");
+      for(U32 i=0;i < (SECTION_WIDTH * SECTION_HEIGHT); i++)
+      {
+        U32 v = 0;
+        data = readUInt(data, &v);
+        data = skipToDigit(data);
+
+        if (v > 820)
+        {
+          section->col[i] = v - 820;
+          section->non[i] = 0;
+        }
+        else
+        {
+          section->col[i] = 0;
+          section->non[i] = v - 1;
+        }
+
+      }
+    }
+    else
+      break;
+  }
+
+  SECTION_DATA_COUNT = sectionIndex;
 }
 
 void Init(Settings* settings)
@@ -90,37 +165,14 @@ void Init(Settings* settings)
   Input_BindKey(SDL_SCANCODE_R, AC_WAIT);
 
   Font_Load("NeoSans.png", &FONT_NEOSANS, Colour_Make(0,0,255), Colour_Make(255,0,255));
-  Bitmap_Load("cave.png", &SPRITESHEET, 0);
-  Bitmap_Load("cats.png", &CATSHEET, 16);
-  Bitmap_Load("player.png", &PLAYERSHEET, 16);
-  Bitmap_Load("coco.png", &COCO, 255);
   Bitmap_Load("Humanoid0.png", &SPRITESHEET_HUMANOID[0], 0);
   Bitmap_Load("Humanoid1.png", &SPRITESHEET_HUMANOID[1], 0);
   Bitmap_Load("Reptile0.png",  &SPRITESHEET_REPTILE[0], 0);
   Bitmap_Load("Reptile1.png",  &SPRITESHEET_REPTILE[1], 0);
+  Bitmap_Load("Wall.png", &SPRITESHEET_WALLS, 0);
+  Bitmap_Load("Floor.png", &SPRITESHEET_FLOORS, 0);
 
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_PLAYER_IDLE, &PLAYERSHEET, 1, 100, 0, 0, 46, 50);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_PLAYER_WALK, &PLAYERSHEET, 8, 120, 0, 150, 46, 50);
-
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT1_IDLE, &CATSHEET, 1, 100, 0, 0, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT1_WALK, &CATSHEET, 3, 100, 0, 0, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT2_IDLE, &CATSHEET, 1, 100, 0, 18, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT2_WALK, &CATSHEET, 3, 100, 0, 18, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT3_IDLE, &CATSHEET, 1, 100, 0, 36, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT3_WALK, &CATSHEET, 3, 100, 0, 36, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT_SHADOW_IDLE, &CATSHEET, 1, 100, 0, 234, 18, 18);
-  Animation_LoadHorizontal(&ANIMATEDSPRITE_CAT_SHADOW_WALK, &CATSHEET, 3, 100, 0, 234, 18, 18);
-
-  Sound_Load(&SOUND_HURT1,   "hurt1.wav");
-  Sound_Load(&SOUND_HURT2,   "hurt2.wav");
-  Sound_Load(&SOUND_JUMP1,   "jump1.wav");
-  Sound_Load(&SOUND_JUMP2,   "jump2.wav");
-  Sound_Load(&SOUND_PICKUP1, "pickup1.wav");
-  Sound_Load(&SOUND_PICKUP2, "pickup2.wav");
-  Sound_Load(&SOUND_SELECT,  "select.wav");
-
-  Bitmap_Load("tiles1.png", &TILES1, 16);
-
+  LoadSectionData();
 }
 
 void Tile_Draw(Bitmap* bitmap, S32 x, S32 y, U32 ox, U32 oy)
@@ -141,6 +193,35 @@ void Tile_Draw(Bitmap* bitmap, S32 x, S32 y, U32 ox, U32 oy)
   Canvas_Splat3(bitmap, &dst, &src);
 }
 
+void Section_Draw(int index)
+{
+  SectionData* section = &SECTION_DATA[index]; 
+
+  for(u32 x=0;x < SECTION_WIDTH;x++)
+  {
+    for(u32 y=0;y < SECTION_HEIGHT;y++)
+    {
+      u32 idx = x + (y * SECTION_WIDTH);
+
+      if (section->col[idx] > 0)
+      {
+        u32 s = section->col[idx];
+        u32 sx = (s % 20);
+        u32 sy = (s / 20);
+        Tile_Draw(&SPRITESHEET_WALLS, x * TILE_SIZE, y * TILE_SIZE, sx, sy);
+      }
+      else
+      {
+        u32 s = section->non[idx];
+        u32 sx = (s % 21);
+        u32 sy = (s / 21);
+        Tile_Draw(&SPRITESHEET_FLOORS, x * TILE_SIZE, y * TILE_SIZE, sx, sy);
+      }
+    }
+  }
+
+}
+
 typedef struct
 {
   s32 x, y;
@@ -159,6 +240,7 @@ typedef struct
   Object player;
   Object objects[MAX_MONSTERS];
   u32    nbObjects;
+  u32    section;
 } Game;
 
 Game*      GAME;
@@ -174,6 +256,20 @@ enum
   OT_AirDragon,
 };
 
+bool CanCollide(u32 x, u32 y)
+{
+  u32 idx = x + (y * SECTION_WIDTH);
+  SectionData* section = &SECTION_DATA[GAME->section];
+  if (section->col[idx])
+  {
+    return true;
+  }
+  else
+  {
+    return section->non[idx] < 45;
+  }
+}
+
 Bitmap* Object_GetBitmap(u8 type)
 {
   switch(type)
@@ -182,7 +278,7 @@ Bitmap* Object_GetBitmap(u8 type)
     case OT_FireDragon:  return &SPRITESHEET_REPTILE[AnimationTimer % 2];
     case OT_WaterDragon: return &SPRITESHEET_REPTILE[AnimationTimer % 2];
     case OT_EarthDragon: return &SPRITESHEET_REPTILE[AnimationTimer % 2];
-    case OT_AirDragon:  return &SPRITESHEET_REPTILE[AnimationTimer % 2];
+    case OT_AirDragon:   return &SPRITESHEET_REPTILE[AnimationTimer % 2];
   }
   return 0;
 }
@@ -273,6 +369,9 @@ bool Object_Tick(Object* object)
     if (ty >= SECTION_WIDTH)
       canMove = false;
 
+    if (CanCollide(tx, ty))
+      canMove = false;
+
     if (canMove)
     {
       for(u32 i=0;i < GAME->nbObjects;i++)
@@ -316,21 +415,17 @@ void RestartLevel()
 
   Player_New(SECTION_WIDTH / 2, SECTION_HEIGHT / 2);
 
-  for(int i=0;i < 4;i++)
+  for(u32 d=0;d < 4;d++)
   {
-    Monster_New(OT_FireDragon, randr(0, SECTION_WIDTH - 1), randr(0, SECTION_HEIGHT - 1));
-  }
-  for(int i=0;i < 4;i++)
-  {
-    Monster_New(OT_WaterDragon, randr(0, SECTION_WIDTH - 1), randr(0, SECTION_HEIGHT - 1));
-  }
-  for(int i=0;i < 4;i++)
-  {
-    Monster_New(OT_EarthDragon, randr(0, SECTION_WIDTH - 1), randr(0, SECTION_HEIGHT - 1));
-  }
-  for(int i=0;i < 4;i++)
-  {
-    Monster_New(OT_AirDragon, randr(0, SECTION_WIDTH - 1), randr(0, SECTION_HEIGHT - 1));
+    for(int i=0;i < 4;i++)
+    {
+      u32 x = randr(0, SECTION_WIDTH - 1);
+      u32 y = randr(0, SECTION_HEIGHT - 1);
+      if (!CanCollide(x, y))
+      {
+        Monster_New(OT_FireDragon + d, x, y);
+      }
+    }
   }
 }
 
@@ -377,6 +472,8 @@ void Step()
     wait = true;
   }
 
+  Section_Draw(0);
+
   if (move || wait)
   {
     bool didMove = Object_Tick(&GAME->player);
@@ -416,16 +513,13 @@ void Step()
     }
   }
 
-
-
   FrameCount++;
   AnimationTimer += (FrameCount % 10 == 0 ? 1 : 0);
   Object_Draw(&GAME->player);
+
   for(u32 i=0; i < GAME->nbObjects;i++)
   {
     Object_Draw(&GAME->objects[i]);
   }
-
-
 
 }
